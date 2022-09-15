b570fe627500e2079ad697b73085b0beec6d7637
Alec Andrews <alecandrews2017@gmail.com>
2022-09-15 14:35:30 -0400

    add pseudocode

---
+++ script.js
@@ -0,0 +1,34 @@
+const number = document.querySelectorAll("number")
+const operator = document.querySelectorAll("operator")
+const results = document.querySelector("results")
+const operand = document.querySelector("operand")
+const equals = document.querySelection("equals")
+const decimal = document.querySelection("decimal")
+const deleteButton = document.querySelection("delete")
+const clear = document.querySelection("clear")
+
+
+
+function addition(a, b) {
+    return a + b
+}
+
+function subtraction(a, b) {
+    return a - b
+}
+
+function multiplication(a, b) {
+    return a * b
+}
+
+function division(a, b) {
+    return a / b
+}
+
+function deletefunc() {
+
+}
+
+function clearfunc() {
+
+}
